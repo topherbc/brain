b8fc99a00313742f9c037dff0a722557b60a8895
Topher <topherbyron@duck.com>
2024-12-19 09:29:34 -0500

    Update __init__.py to use data_formatter tool instance

--- brain/tools/__init__.py
+++ brain/tools/__init__.py
@@ -1,9 +1,9 @@
 from .text_cleaner import text_cleaner, tokenizer
-from .data_formatter import DataFormatter
+from .data_formatter import data_formatter
 from .analyzers import RiskEvaluator, PatternAnalyzer
 from .memory import MemoryDatabase
 from .confidence import ConfidenceAnalyzer
 from .base import BaseTool
 
 # List of all available tools
-tools = [text_cleaner, tokenizer]
+tools = [text_cleaner, tokenizer, data_formatter]
