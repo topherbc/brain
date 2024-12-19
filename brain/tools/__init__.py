dadde90f694463f7e19ef661851007e91f210e7d
Topher <topherbyron@duck.com>
2024-12-19 09:30:54 -0500

    Update __init__.py to use memory tool instances

--- brain/tools/__init__.py
+++ brain/tools/__init__.py
@@ -1,9 +1,17 @@
 from .text_cleaner import text_cleaner, tokenizer
 from .data_formatter import data_formatter
 from .analyzers import RiskEvaluator, PatternAnalyzer
-from .memory import MemoryDatabase
+from .memory import memory_store, memory_retrieve, memory_update, memory_delete
 from .confidence import ConfidenceAnalyzer
 from .base import BaseTool
 
 # List of all available tools
-tools = [text_cleaner, tokenizer, data_formatter]
+tools = [
+    text_cleaner,
+    tokenizer,
+    data_formatter,
+    memory_store,
+    memory_retrieve,
+    memory_update,
+    memory_delete
+]
